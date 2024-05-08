ef395ed4b4e1b7d867b81d52f04d90dd565229bd
Jeff Chen <r09942097@ntu.edu.tw>
2024-05-08 21:24:39 +0800

    _

--- _merger.sh
+++ _merger.sh
@@ -6,3 +6,12 @@ git add -A
 git commit -m \
 'merged at '"$(date)"
 git push
+
+git fetch overleaf
+git fetch github
+git merge overleaf/master
+git merge github/master
+git add -A
+git commit -m \
+'merged at '"$(date)"
+git push
