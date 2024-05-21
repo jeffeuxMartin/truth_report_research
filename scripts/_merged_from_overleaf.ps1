9040c115b2b23b59f16a370956d3c34e95fc4726
Jeff Chen <you@example.com>
2024-05-21 10:06:15 +0800

    add local modification at 2024/5/21 10:06:15

---
+++ scripts/_merged_from_overleaf.ps1
@@ -0,0 +1,36 @@
+Set-Location C:\Users\ChienChengChen\Desktop\truth_report_research
+
+git add -A
+git commit -m ('add local modification at ' + (Get-Date).ToString())
+git push
+
+git fetch origin
+# git fetch overleaf
+try {
+    Start-Process -FilePath "git" -ArgumentList "fetch overleaf" -Wait -Timeout 3000
+    # Write-Output "fetched origin"
+}
+catch {
+    Write-Output "failed to fetch overleaf"
+    exit 97
+}
+git pull overleaf --rebase
+git merge overleaf/master
+# git merge origin/master
+git add -A
+git commit -m `
+'merged from Overleaf at '"$(Get-Date)"
+git push
+
+# git fetch overleaf
+# git fetch origin
+# git merge overleaf/master
+# git merge origin/master
+# git add -A
+# git commit -m \
+# 'merged at '"$(date)"
+# git push
+
+git fetch overleaf
+git fetch github
+git fetch origin
