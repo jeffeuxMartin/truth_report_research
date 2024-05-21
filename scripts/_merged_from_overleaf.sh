525999cd8290bb4fa1b35311df9f707e0164d042
Jeff Chen <r09942097@ntu.edu.tw>
2024-05-21 08:25:33 +0800

    merged from Overleaf at 西元2024年05月21日 (週二) 08時25分33秒 CST

---
+++ scripts/_merged_from_overleaf.sh
@@ -0,0 +1,18 @@
+cd /storage/LabJob/Projects/truth_report_research
+# git fetch origin
+git fetch overleaf
+git merge overleaf/master
+# git merge origin/master
+git add -A
+git commit -m \
+'merged from Overleaf at '"$(date)"
+git push
+
+# git fetch overleaf
+# git fetch origin
+# git merge overleaf/master
+# git merge origin/master
+# git add -A
+# git commit -m \
+# 'merged at '"$(date)"
+# git push
