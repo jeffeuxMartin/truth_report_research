7685fea4e8a2c03990420dce39ebcfc73ea63af9
Jeff Chen <r09942097@ntu.edu.tw>
2024-05-21 08:58:21 +0800

    merged at 西元2024年05月21日 (週二) 08時58分21秒 CST

--- scripts/_merger.sh
+++ scripts/_merger.sh
@@ -1,8 +1,10 @@
 cd /storage/LabJob/Projects/truth_report_research
 git fetch origin
+git fetch github
 git fetch overleaf
-git merge overleaf/master
 git merge origin/master
+git merge github/master
+git merge overleaf/master
 git add -A
 git commit -m \
 'merged at '"$(date)"
@@ -10,8 +12,10 @@ git push
 
 git fetch overleaf
 git fetch origin
+git fetch github
 git merge overleaf/master
 git merge origin/master
+git merge github/master
 git add -A
 git commit -m \
 'merged at '"$(date)"
