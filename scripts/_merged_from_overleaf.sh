8dab300cf28728846362fffbe99be45ee0d94c97
Jeff Chen (GCloud) <jeffeuxmlta2020@gmail.com>
2024-05-24 11:40:38 +0800

    add local modification at Fri May 24 11:40:38 AM CST 2024

--- scripts/_merged_from_overleaf.sh
+++ scripts/_merged_from_overleaf.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+if [[ -d /storage/LabJob/Projects/truth_report_research ]]; then
 cd /storage/LabJob/Projects/truth_report_research
+else
+cd /home/jeffeuxmlta2020/truth_report_research
+fi
 
 git add -A
 git commit -m \
